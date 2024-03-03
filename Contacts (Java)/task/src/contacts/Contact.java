2db438f3e051fef08661e1831f7cbde1e95d8f78
ProkurorNSK <fedyashin.pavel@yandex.ru>
2024-03-04 02:31:35 +0700

    1

---
+++ Contacts (Java)/task/src/contacts/Contact.java
@@ -0,0 +1,13 @@
+package contacts;
+
+class Contact {
+    String name;
+    String surname;
+    String number;
+
+    public Contact(String name, String surname, String number) {
+        this.name = name;
+        this.surname = surname;
+        this.number = number;
+    }
+}
--- Contacts (Java)/task/src/contacts/Main.java
+++ Contacts (Java)/task/src/contacts/Main.java
@@ -1,7 +1,19 @@
 package contacts;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the name of the person:");
+        String name = sc.nextLine();
+        System.out.println("Enter the surname of the person:");
+        String surname = sc.nextLine();
+        System.out.println("Enter the number:");
+        String number = sc.nextLine();
+
+        new Contact(name, surname, number);
+        System.out.println("A record created!");
+        System.out.println("A Phone Book with a single record created!");
     }
 }

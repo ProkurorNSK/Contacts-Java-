da1828df046a6ffd018275c1757203be835adfd0
ProkurorNSK <fedyashin.pavel@yandex.ru>
2024-03-06 22:24:18 +0700

    2

--- Contacts (Java)/task/src/contacts/Contact.java
+++ Contacts (Java)/task/src/contacts/Contact.java
@@ -1,13 +1,61 @@
 package contacts;
 
 class Contact {
-    String name;
-    String surname;
-    String number;
+    private String name;
+    private String surname;
+    private String number;
 
-    public Contact(String name, String surname, String number) {
+    private Contact(String name, String surname, String number) {
         this.name = name;
         this.surname = surname;
         this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + surname + ", " + (number.isEmpty() ? "[no number]" : number);
+    }
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    void setNumber(String number) {
+        this.number = number;
+    }
+
+    static class Builder {
+        private String name;
+        private String surname;
+        private String number;
+
+        Builder() {
+            name = "";
+            surname = "";
+            number = "";
+        }
+
+        Contact build() {
+            return new Contact(name, surname, number);
+        }
+
+        Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        Builder setSurname(String surname) {
+            this.surname = surname;
+            return this;
+        }
+
+        Builder setNumber(String number) {
+            this.number = number;
+            return this;
+        }
     }
 }
--- Contacts (Java)/task/src/contacts/Main.java
+++ Contacts (Java)/task/src/contacts/Main.java
@@ -1,19 +1,92 @@
 package contacts;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the name of the person:");
-        String name = sc.nextLine();
-        System.out.println("Enter the surname of the person:");
-        String surname = sc.nextLine();
-        System.out.println("Enter the number:");
-        String number = sc.nextLine();
 
-        new Contact(name, surname, number);
-        System.out.println("A record created!");
-        System.out.println("A Phone Book with a single record created!");
+    private static final List<Contact> contacts = new LinkedList<>();
+    private static final Scanner sc = new Scanner(System.in);
+
+    private static final String numberFormatFilter = "\\+?(\\([0-9a-zA-Z]+\\)|[0-9a-zA-Z]+([ -][(][0-9a-zA-Z]{2,}[)])?)([ -][0-9a-zA-Z]{2,})*";
+
+    public static void main(String[] args) {
+        String command;
+
+        while (true) {
+            System.out.print("Enter action (add, remove, edit, count, list, exit): ");
+            command = sc.nextLine();
+            switch (command) {
+                case "add" -> addContact();
+                case "remove" -> removeContact();
+                case "edit" -> editContact();
+                case "count" -> System.out.printf("The Phone Book has %d records.\n", contacts.size());
+                case "list" -> printContacts();
+                case "exit" -> {
+                    return;
+                }
+            }
+        }
+    }
+
+    private static void editContact() {
+        if (contacts.isEmpty()) {
+            System.out.println("No records to edit!");
+        } else {
+            printContacts();
+            System.out.print("Select a record: ");
+            String input = sc.nextLine();
+            Contact contact = contacts.get(Integer.parseInt(input) - 1);
+            System.out.print("Select a field (name, surname, number): ");
+            input = sc.nextLine();
+            System.out.printf("Enter %s: ", input);
+            String field = sc.nextLine();
+            switch (input) {
+                case "name" -> contact.setName(field);
+                case "surname" -> contact.setSurname(field);
+                case "number" -> {
+                    if (!field.matches(numberFormatFilter)) {
+                        field = "";
+                        System.out.println("Wrong number format!");
+                    }
+                    contact.setNumber(field);
+                }
+            }
+            System.out.println("The record updated!");
+        }
+    }
+
+    private static void printContacts() {
+        for (int i = 1; i <= contacts.size(); i++) {
+            System.out.printf("%d. %s\n", i, contacts.get(i - 1));
+        }
+    }
+
+    private static void removeContact() {
+        if (contacts.isEmpty()) {
+            System.out.println("No records to remove!");
+        } else {
+            printContacts();
+            System.out.print("Select a record: ");
+            String input = sc.nextLine();
+            contacts.remove(Integer.parseInt(input) - 1);
+            System.out.println("The record removed!");
+        }
+    }
+
+    private static void addContact() {
+        System.out.print("Enter the name of the person: ");
+        String name = sc.nextLine();
+        System.out.print("Enter the surname of the person: ");
+        String surname = sc.nextLine();
+        System.out.print("Enter the number: ");
+        String number = sc.nextLine();
+        if (!number.matches(numberFormatFilter)) {
+            number = "";
+            System.out.println("Wrong number format!");
+        }
+        contacts.add(new Contact.Builder().setName(name).setSurname(surname).setNumber(number).build());
+        System.out.println("The record added.");
     }
 }
